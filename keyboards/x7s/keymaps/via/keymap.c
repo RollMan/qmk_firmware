465f7aee69c54376df5fa3662403b74c01d6676b
Yohei Shimmyo <qwpmb554@gmail.com>
2025-01-18 17:45:21 +0900

    move mouse wheels assignments left

--- keyboards/x7s/keymaps/via/keymap.c
+++ keyboards/x7s/keymaps/via/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
         RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,   KC_NO,
         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,
-        KC_NO,   KC_RALT, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
+        KC_NO,   KC_RALT, KC_NO,   KC_NO,   KC_NO,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,
         XXXXXXX, XXXXXXX, KC_NO,   KC_NO,   KC_NO,   KC_BTN2, KC_BTN1, KC_BTN3, XXXXXXX, XXXXXXX
     ),
 
